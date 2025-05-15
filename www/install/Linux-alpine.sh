4f53ccf748ebc1822caaa539a6b1e043773b8bb9
Alan Gutierrez <alan@prettyrobots.com>
2025-05-14 21:40:38 -0500

    Adjust installers.

--- www/install/Linux-alpine.sh
+++ www/install/Linux-alpine.sh
@@ -5,14 +5,9 @@ if [[ "$(id -u)" != 0 ]]; then
     exit 1
 fi
 
-if grep -q '^https://zshctl/apk$' /etc/apk/repositories; then
-    echo 'error: arectl already in `/etc/apk/repositories`, giving up'
-    exit 1
+if ! grep -q '^https://zshctl.sh/apk$' /etc/apk/repositories; then
+    echo https://zshctl.sh/apk >> /etc/apk/repositories
 fi
-
-cat <<EOF >> /etc/apk/repositories
-https://zshctl.sh/apk
-EOF
 
 wget -qO /etc/apk/keys/alan@prettyrobots.com-67eee297.rsa.pub https://zshctl.sh/keys/alan@prettyrobots.com-67eee297.rsa.pub
 
@@ -20,4 +15,4 @@ apk --no-progress update
 apk --no-progress add zshctl
 
 type zshctl
-/usr/bin/zshctl version
+/usr/bin/zshctl version --format verbose
--- www/install/Linux-ubuntu.bash
+++ www/install/Linux-ubuntu.bash
@@ -17,8 +17,9 @@ if ! type gpg > /dev/null 2>&1; then
     apt-get install -y gnupg2
 fi
 
-wget -qO - https://zshctl.sh/keys/gpg | apt-key --keyring /usr/share/keyrings/zshctl.sh.gpg add -
-echo "deb [arch=all signed-by=/usr/share/keyrings/zshctl.sh.gpg] https://zshctl.sh/apt stable main" > /etc/apt/sources.list.d/zshctl.list
+wget -qO - https://acrectl.sh/keys/gpg |
+    sudo gpg --yes --dearmor -o /usr/share/keyrings/zshctl.gpg
+echo "deb [arch=all signed-by=/usr/share/keyrings/zshctl.gpg] https://zshctl.sh/apt stable main" > /etc/apt/sources.list.d/zshctl.list
 apt-get update
 apt-get install -y zshctl
 
