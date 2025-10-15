6832c3105f6b966fa53e937c93bbe56ed3eade59
Alan Gutierrez <alan@prettyrobots.com>
2025-10-15 15:21:50 -0500

    Indent.

--- share/zshctl/zshctl/commands/version/command.zsh
+++ share/zshctl/zshctl/commands/version/command.zsh
@@ -2,23 +2,23 @@ zmodload zsh/datetime
 
 function :help:version {
     heredoc -v help -q <<'    EOF'
-    # terse
-    display version
-    # verbose
-    Display the current version of \`${zshctl[program]}\`.
-    # arg format -- < terse | verbose | shell | json >
-    Display a verbose version as test, JSON, or shell escaped.
-        * terse -- Display version number only.
-        * verbose -- Display version number and release date.
-        * shell -- Display shell esacped version number and release date as UNIX epoch.
-        * json -- Display version number and release date as UNIX epoch as JSON.
-    # arg help
-    Help for \`${zshctl[program]} version\`.
-    # man
-    ## DESCRIPTION
-    Displays the current version of \`${zshctl[program]}\`.
-    ## OPTIONS
-    ### options
+        # terse
+        display version
+        # verbose
+        Display the current version of \`${zshctl[program]}\`.
+        # arg format -- < terse | verbose | shell | json >
+        Display a verbose version as test, JSON, or shell escaped.
+            * terse -- Display version number only.
+            * verbose -- Display version number and release date.
+            * shell -- Display shell escaped version number and release date as UNIX epoch.
+            * json -- Display version number and release date as UNIX epoch as JSON.
+        # arg help
+        Help for \`${zshctl[program]} version\`.
+        # man
+        ## DESCRIPTION
+        Displays the current version of \`${zshctl[program]}\`.
+        ## OPTIONS
+        ### options
     EOF
 }
 
