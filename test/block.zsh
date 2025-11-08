ef54eb462be190ccec2867d3ad4a76908cd20680
Alan Gutierrez <alan@prettyrobots.com>
2025-11-08 11:42:56 -0600

    Resurrect block, use without show and indent.
    Updated `block` for use without show or indent so you can now write
    programs that execute commands and capture their errors with a context
    message and print them to the user. This is the tool-native error
    reporting that can now be used in general programs, not just pipeline
    automations. To skip the banner and the indent use `block -i 0`, where
    indent zero implies no banner.

---
+++ test/block.zsh
@@ -0,0 +1,50 @@
+fpath=( share/zshctl/functions $fpath )
+autoload -zU warn abend heredoc slurp block
+
+function bad {
+    print something bad is about to happen
+    print something bad happend 1>&2
+    print bad 1>&2
+    print very bad 1>&2
+    return 8
+}
+
+function {
+    if false; then
+    block -i 0
+    {
+        try print hello || tried
+    } always {
+        caught
+    }
+    (
+        block -i 0
+        {
+            try bad || tried 'bad error: something bad happened'
+        } always {
+            caught
+        }
+    )
+    (
+        block -i 0
+        {
+            typeset json
+            try && json=$(print '{}') || tried 'bad error: something bad happened'
+            print $json
+        } always {
+            caught
+        }
+    )
+    fi
+    (
+        block -i 0
+        {
+            typeset try
+            try -v try && json=$(bad 2>&$try) ||
+                tried -c 99 'bad error: something bad happened'
+        } always {
+            caught
+        }
+    )
+    print $?
+}
